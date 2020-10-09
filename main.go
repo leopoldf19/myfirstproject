f0d295ccc29aa051d69b02c9288df17456903fce
LeopoldFuchs <leopold.fuchs@ibm.com>
2020-10-09 11:50:17 +0200

    added templates and data transfer

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
-	"io"
+	"html/template"
 	"net/http"
 )
 
+var templ *template.Template
+
+func init() {
+	templ = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World")
+	templ.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func registriertHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "Post" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	vorname := r.FormValue("vn")
+	nachname := r.FormValue("nn")
+
+	data := struct {
+		vn string
+		nn string
+	}{
+		vn: vorname,
+		nn: nachname,
+	}
+
+	templ.ExecuteTemplate(w, "registriert.gohtml", data)
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/registriert", registriertHandler)
 	http.ListenAndServe(":8080", nil)
 }
